def3a2ddcfa91b2d4065c1cf4a8378757cd2bf7e
Aryan Mishra <mishraa92799@gmail.com>
2025-07-17 07:26:04 +0530

    Add files via upload

---
+++ remove element.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+
+       for(int i=0;i<nums.size();i++)
+       {
+        if(nums[i]==val)
+        {
+            nums.erase(remove(nums.begin(),nums.end(),val),nums.end());
+        }
+       }
+        int count=0;
+
+       return nums.size();
+    }
+};
