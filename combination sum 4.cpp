5037447b0556254ef00fa0133fd3da1e361507ea
Aryan Mishra <mishraa92799@gmail.com>
2025-07-30 20:04:09 +0530

    Add files via upload

---
+++ combination sum 4.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+int solve(vector<int> & num,int tar, vector<int>&dp)
+{
+    if(tar<0)
+    {
+        return 0;
+    }
+    if(tar==0)
+    {
+        return 1;
+    }
+    if(dp[tar]!=-1)
+    {
+        return dp[tar];
+    }
+    int ans=0;
+    for(int i=0;i<num.size();i++)
+    {
+        ans=ans+solve(num,tar-num[i],dp);
+    }
+    dp[tar]=ans;
+    return dp[tar];
+}
+int findWays(vector<int> &num, int tar)
+{
+    // Write your code here.
+    vector<int> dp(tar+1,-1);
+    return solve(num,tar,dp);
+};
