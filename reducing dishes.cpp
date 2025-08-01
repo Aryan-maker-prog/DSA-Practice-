fd3bce220af4e85c2462ff1e5de75f415aba52d1
Aryan Mishra <mishraa92799@gmail.com>
2025-08-01 11:54:27 +0530

    Add files via upload

---
+++ reducing dishes.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int solve(vector<int> & satisfaction,int index,int time,vector<vector<int>>&dp)
+    {
+        if(index==satisfaction.size())
+        {
+            return 0;
+        }
+        if(dp[index][time]!=-1)
+        {
+            return dp[index][time];
+        }
+        int incl=satisfaction[index]*(time+1)+solve(satisfaction,index+1,time+1,dp);
+        int excl=0+solve(satisfaction,index+1,time,dp);
+        dp[index][time]= max(incl,excl);
+        return dp[index][time];
+
+    }
+    int maxSatisfaction(vector<int>& satisfaction) {
+
+        sort(satisfaction.begin(),satisfaction.end());
+        int n=satisfaction.size();
+        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
+       return solve(satisfaction,0,0,dp);
+    }
+};
